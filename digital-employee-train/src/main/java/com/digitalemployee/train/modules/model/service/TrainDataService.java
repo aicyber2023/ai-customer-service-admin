cd6fe3d8ba932f054ba20d4ff66e9de3250898b4
xz <xiangzheng@aicyber.com>
2024-01-05 10:29:07 +0800

    fix: 调整文件目录

--- digital-employee-train/src/main/java/com/digitalemployee/train/modules/model/service/TrainDataService.java
+++
@@ -1,7 +0,0 @@
-package com.digitalemployee.train.modules.model.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface TrainDataService {
-    String extractTrainData(MultipartFile[] files, String prompt);
-}
